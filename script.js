b5c8f7065c4e7038818e392a6dbd65f4145435a6
Vinh Ngo <117776860+vinhngo380@users.noreply.github.com>
2024-07-20 13:55:27 -0400

    Procedurally create divs for the grid

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-
 function createGrid() {
+	const mainDiv = document.getElementById("main");
 	for (let i = 0; i < 4; i++) {
-		rowDiv = document.createElement("div");
+		let rowDiv = document.createElement("div");
+		rowDiv.id = "row" + i;
+		rowDiv.classList.add("row-container");
+		mainDiv.appendChild(rowDiv)
 		for (let j = 0; j < 4; j++) {
-
+			let squareDiv = document.createElement("div");
+			let squareIDNumber = (i + 1) * (j + 1);
+			squareDiv.id = "square" + squareIDNumber;
+			squareDiv.classList.add("row-container");
+			rowDiv.appendChild(squareDiv);
 		}
 	}
 }
+
+function main() {
+	createGrid()
+}
+
+main()
