48542656a10b61ed926bf4c4a853525af4817365
Vinh Ngo <117776860+vinhngo380@users.noreply.github.com>
2024-07-21 11:50:21 -0400

    Shortened some lines

--- script.js
+++ script.js
@@ -17,7 +17,10 @@ function createGrid(gridSize = 4) {
 			let squareIDNumber = (i + 1) * (j + 1);
 			squareDiv.id = "square" + squareIDNumber;
 			squareDiv.classList.add("square");
-			squareDiv.setAttribute("style", `height:${squareSize}px; width:${squareSize}px;`);
+			squareDiv.setAttribute(
+				"style", 
+				`height:${squareSize}px; width:${squareSize}px;`
+			);
 			squareDiv.style.opacity = 0;
 			squareDiv.addEventListener("mouseover", (event) => {
 				squareDiv.style.backgroundColor = "white";
@@ -33,7 +36,8 @@ function colorMyPencils(squareDiv) {
 	if (currentOpa < 1) {
 		currentOpa += 0.1;
 		squareDiv.style.opacity = currentOpa;
-		let index = Math.floor(Math.random() * (globalVals.colors.length + 1));
+		let colorsLength = globalVals.colors.length + 1;
+		let index = Math.floor(Math.random() * (colorsLength));
 		squareDiv.style.background = globalVals.colors[index];
 	}
 }
@@ -48,9 +52,10 @@ function clearMainDiv() {
 function main() {
 	const newGridButton = document.getElementById("new-grid-button");
 	newGridButton.addEventListener("click", (event) => {
-		let gridSize = prompt("Enter a new grid size (less than 100)");
+		let promptText = "Enter a new grid size (less than 100)";
+		let gridSize = prompt(promptText);
 		while (gridSize >= 100) {
-			gridSize = prompt("Enter a new grid size (less than 100)");
+			gridSize = prompt(promptText);
 		}
 		clearMainDiv();
 		createGrid(parseInt(gridSize));
